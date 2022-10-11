33b8c6957049c5688820c37a07c4c55d4d53839c
徐文强 <xuwenqiang@sanzhi56.cn>
2022-10-11 14:47:26 +0800

    push

---
+++ pages/home/home.vue
@@ -0,0 +1,29 @@
+<template>
+	<view>
+		<view>
+			<swiper indicator-dots circular>
+				<swiper-item>A</swiper-item>
+				<swiper-item>B</swiper-item>
+				<swiper-item>C</swiper-item>
+				<swiper-item>D</swiper-item>
+			</swiper>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ unpackage/dist/dev/mp-weixin/pages/index/index.js
@@ -0,0 +1,26 @@
+"use strict";
+var common_vendor = require("../../common/vendor.js");
+const _sfc_main = {
+  data() {
+    return {
+      title: "Hello"
+    };
+  },
+  onLoad() {
+  },
+  methods: {
+    login() {
+      common_vendor.index.redirectTo({
+        url: "/pages/home/home"
+      });
+    }
+  }
+};
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return {
+    a: common_vendor.t($data.title),
+    b: common_vendor.o((...args) => $options.login && $options.login(...args))
+  };
+}
+var MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "D:/wxapp/feiPan/pages/index/index.vue"]]);
+wx.createPage(MiniProgramPage);
